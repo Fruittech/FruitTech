f61078724378b17f1371584a1bb45265e4a2c7c9
Isaac <Isaac.silvajr@sptech.school>
2023-12-02 23:36:16 -0300

    Login Funcionario finalizado

--- fruittech/src/controllers/funcionarioController.js
+++ fruittech/src/controllers/funcionarioController.js
@@ -50,65 +50,32 @@ function autenticar(req, res) {
   }
 }
 
-function cadastrar(req, res) {
-  // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-  var nome = req.body.nomeServer;
-  var email = req.body.emailServer;
-  var senha = req.body.senhaServer;
-  var empresaId = req.body.empresaServer;
-
-  // Faça as validações dos valores
-  if (nome == undefined) {
-    res.status(400).send("Seu nome está undefined!");
-  } else if (email == undefined) {
-    res.status(400).send("Seu email está undefined!");
-  } else if (senha == undefined) {
-    res.status(400).send("Sua senha está undefined!");
-  } else if (empresaId == undefined) {
-    res.status(400).send("Sua empresa está undefined!");
-  } else {
-    // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-    usuarioModel
-      .cadastrar(nome, email, senha, empresaId)
-      .then(function (resultado) {
-        res.json(resultado);
-      })
-      .catch(function (erro) {
-        console.log(erro);
-        console.log(
-          "\nHouve um erro ao realizar o cadastro! Erro: ",
-          erro.sqlMessage
-        );
-        res.status(500).json(erro.sqlMessage);
-      });
-  }
-}
-
 function cadastrarFuncionario(req, res) {
   // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
-  var nome = req.body.nomeServer;
-  var sobrenome = req.body.sobrenomeServer;
-  var cpf = req.body.cpfServer;
-  var senha = req.body.senhaServer;
-  var id = req.body.idServer;
+  var nome = req.body.nomeServer
+  var sobrenome = req.body.sobrenomeServer
+  var cpf = req.body.cpfServer
+  var senha = req.body.senhaServer
+  var idT = req.params.idTransportadora
 
   // Faça as validações dos valores
   if (nome == undefined) {
     res.status(400).send("Seu nome está undefined!");
   } else if (sobrenome == undefined) {
-    res.status(400).send("Seu email está undefined!");
+    res.status(400).send("Seu sobrenome está undefined!");
+  } else if (cpf == undefined) {
+    res.status(400).send("Seu CPF está undefined!");
   } else if (senha == undefined) {
     res.status(400).send("Sua senha está undefined!");
-  } else if (cpf == undefined) {
-    res.status(400).send("Sua empresa está undefined!");
-  } else if (id == undefined) {
-    res.status(400).send("Sua empresa está undefined!");
+  } else if (idT == undefined) {
+    res.status(400).send("Seu idTransportadora está undefined!");
   } else {
     // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-    funcionarioModel
-      .cadastrarFuncionario(nome, sobrenome, cpf, senha, id)
+
+    funcionarioModel.cadastrarFuncionario(nome, sobrenome, cpf, senha, idT)
       .then(function (resultado) {
         res.json(resultado);
+
       })
       .catch(function (erro) {
         console.log(erro);
@@ -118,11 +85,12 @@ function cadastrarFuncionario(req, res) {
         );
         res.status(500).json(erro.sqlMessage);
       });
+
   }
 }
 
+
 module.exports = {
   autenticar,
-  cadastrar,
   cadastrarFuncionario,
 };
